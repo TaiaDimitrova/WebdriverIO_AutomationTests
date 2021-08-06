23aa793d49f6e5500b89e0db99a0f210a9d4e0ad
TaiaDimitrova <taiadt@gmail.com>
2021-08-06 19:45:14 +0300

    Added code format on pre-comit hook

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -4,7 +4,7 @@ import SecurePage from '../pageobjects/secure.page';
 describe('My Login application', () => {
   it('should login with valid credentials', async () => {
     await LoginPage.open();
-
+    await console.log('completed');
     await LoginPage.login('tomsmith', 'SuperSecretPassword!');
     await expect(SecurePage.flashAlert).toBeExisting();
     await expect(SecurePage.flashAlert).toHaveTextContaining('You logged into a secure area!');
